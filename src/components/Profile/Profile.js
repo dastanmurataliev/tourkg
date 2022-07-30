fa358bd5b43f5488939487ac70f151820f1ea533
dastanmurataliev <dastan_murataliev@inbox.ru>
2022-07-30 15:21:12 +0600

    theme Redux state-

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,6 +4,6 @@ export default function Profile({mode, setMode}) {
     return (<div>
         Профиль пользователя
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia dignissimos id, asperiores non et expedita voluptates velit sequi quae voluptatibus similique iste ad, veniam ipsa doloremque accusamus labore! Ut, placeat?</p>
-        <Mode mode={mode} changeMode={setMode}/>    
+        <Mode/>    
     </div>)
 }
